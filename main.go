cad665a3cda88bc3213934c2b0e23cfccca3436f
松岡深徳 <Matsuoka-M@202501-02.local>
2025-02-17 23:16:46 +0900

    feat: fizzbuzz

--- main.go
+++ main.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-    fmt.Println("Hello, Go")
+    for i := 1; i <= 100; i++ {
+        fizzbuzz(i)
+    }
+}
+
+func fizzbuzz(i int) {
+    if i%3 == 0 && i%5 == 0 {
+        fmt.Println("fizzbuzz")
+    } else if i%3 == 0 {
+        fmt.Println("fizz")
+    } else if i%5 == 0 {
+        fmt.Println("buzz")
+    } else {
+        fmt.Println(i)
+    }
 }
